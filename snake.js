f9361263ea48b2a437802a34f8171beafe1d3edf
Aditi002-holo <aditideshpande2908@gmail.com>
2022-09-04 19:43:00 +0530

    snake's direction code complete

--- snake.js
+++ snake.js
@@ -1,17 +1,30 @@
-export const SNAKE_SPEED = 2;
+import { getInputDirection } from "./input.js";
+
+export const SNAKE_SPEED = 5;
 
 // x and y positions - 21 grids so 21/2 ~ 11
-const snakeBody = [{x: 11, y: 11}];
+const snakeBody = [
+    {x: 11, y: 11},
+];
 export function update() {
-    // console.log('update snake')
+    // imports input direction from input js
+    const inputDirection = getInputDirection();
+    for(let i = snakeBody.length - 2; i >= 0; i--) {
+        // moving everything to a new object
+        snakeBody[i+1] = { ...snakeBody[i] };
+
+    }
+
+    snakeBody[0].x += inputDirection.x;
+    snakeBody[0].y += inputDirection.y;
 }
 
 export function draw(gameBoard) {
     snakeBody.forEach(segment => {
         const snakeElement = document.createElement('div');
         // x and y for each snakeBody segment
-        snakeElement.style.gridRowStart = segment.x;
-        snakeElement.style.gridColumnStart = segment.y;
+        snakeElement.style.gridRowStart = segment.y;
+        snakeElement.style.gridColumnStart = segment.x;
 
         // add the class snake - defined in css
         snakeElement.classList.add('snake');
